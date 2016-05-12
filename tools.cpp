2f628b2e5ec01ae5e5e528a9cb6f9c507b9cc52e
raffll <rafal.wierzchos@gmail.com>
2016-05-12 14:55:26 +0200

    Add files via upload

--- tools.cpp
+++ tools.cpp
@@ -1,3 +1,10 @@
 #include "tools.hpp"
 
 using namespace std;
+
+//----------------------------------------------------------
+void tools::setFileName(string &file_name)
+{
+	string slash = "\\/";
+	file_name = file_name.substr(file_name.find_last_of(slash) + 1);
+}
--- tools.hpp
+++ tools.hpp
@@ -3,13 +3,20 @@
 
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
+
+static vector<string> dict_name = {"cell", "gmst", "fnam", "desc", "book", "fact", "indx", "dial", "info", "scpt"};
+static vector<string> key = {"Choice", "choice", "MessageBox", "Say ", "Say,", "say ", "say,"};
+static map<int, string> type_coll = {{0, "T"}, {1, "V"}, {2, "G"}, {3, "P"}, {4, "J"}};
+static vector<string> line_sep = {"<h3>", "</h3>", "<hr>"};
+static string inner_sep = {"^"};
 
 class tools
 {
 public:
-	vector<string> dict_name = {"cell", "gmst", "fnam", "desc", "book", "fact", "indx", "dial", "info", "scpt"};
+	void setFileName(string &file_name);
 };
 
 #endif
