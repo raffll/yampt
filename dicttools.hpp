f31d17e81d485805604549270e13be4eff249636
raffll <rafal.wierzchos@gmail.com>
2016-05-20 13:06:38 +0200

    hide some func

--- dicttools.hpp
+++ dicttools.hpp
@@ -19,16 +19,17 @@ class dicttools : public tools
 public:
 	void readDictAll(const char* path);
 	void readDict(const char* path, int i);
-	void printStatus(int i);
 	void printDict(int i);
-	void parseDict(int i);
-	void validateDict(int i);
-	void validateRecLength(int i);
 	bool getStatus(int i) { return status[i]; }
 
 	dicttools();
 
 private:
+	void printStatus(int i);
+	void parseDict(int i);
+	void validateDict(int i);
+	void validateRecLength(int i);
+
 	array<int, 10> status = {};
 	array<string, 10> file_name;
 	array<string, 10> file_content;
