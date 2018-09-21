a5c58eb0c1cbd1d44f288013f821613cc8c773ab
raffll <rafal.wierzchos@gmail.com>
2018-09-21 10:06:49 +0200

    update paths

--- gui/config.cpp
+++ gui/config.cpp
@@ -2,10 +2,10 @@
 
 //----------------------------------------------------------
 Config::Config() :
-    exec_path("/home/braindead/Projekty/C++/build-yampt-Desktop-Release/yampt"),
-    dict_path("/home/braindead/Dokumenty/Dropbox/Morrowind/dict_base/ENtoPL+PatchPLv2.0.xml"),
-    input_path("/home/braindead/.local/share/openmw"),
-    output_path("/home/braindead/Dokumenty/Dropbox/Morrowind/output")
+    exec_path("yampt"),
+    dict_path("dict_base/*.xml"),
+    input_path("input"),
+    output_path("output")
 {
 
 }
