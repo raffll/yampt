93bb45a09bdc00faad9bab66b44d714441135431
raffll <rafal.wierzchos@gmail.com>
2018-12-10 13:00:13 +0100

    scripts update

--- resources/yampt-generate-report-template.cmd
+++ resources/yampt-generate-report-template.cmd
@@ -20,10 +20,10 @@ for /r "%USER%" %%f in (*.xml) do echo %%f >> tmp1.txt
 
 echo %ECHO_1%
 echo %ECHO_1% >> raport.log
-for /f "delims=" %%x in (tmp1.txt) do ( yampt.exe --merge -l -d %%x | findstr "Loading Duplicate Too Ok Invalid Doubled" | findstr /v /r /c:"Doubled.*Invalid" ) >> raport.log
+for /f "delims=" %%x in (tmp1.txt) do ( yampt.exe --merge -l -d "%%x" | findstr "Loading Duplicate Too Ok Invalid Doubled" | findstr /v /r /c:"Doubled.*Invalid" ) >> raport.log
 echo %ECHO_2%
 echo %ECHO_2% >> raport.log
-for /f "delims=" %%x in (tmp1.txt) do ( yampt.exe --merge -l -d "%DICT_N%" %%x | findstr "Loading Unused" ) >> raport.log
+for /f "delims=" %%x in (tmp1.txt) do ( yampt.exe --merge -l -d "%DICT_N%" "%%x" | findstr "Loading Unused" ) >> raport.log
 
 del tmp1.txt >nul 2>&1
 del Merged.xml >nul 2>&1
--- resources/yampt-make-user-template.cmd
+++ resources/yampt-make-user-template.cmd
@@ -2,30 +2,25 @@
 
 SET _INPUT=input
 SET OUTPUT=output
-SET SUFFIX=
 
 SET BASE=dict_base
-SET USER=dict_user
 SET _NEW=dict_new
 
 SET DICT_N=%BASE%\NATIVE.xml
 SET DICT_F=%BASE%\NATIVE_for_find_changed_only.xml
-
-SET EXCLUDE=
 
 REM ############### DON'T EDIT ###############
 
 mkdir "%_INPUT%" >nul 2>&1
 mkdir "%OUTPUT%" >nul 2>&1
 mkdir "%BASE%" >nul 2>&1
-mkdir "%USER%" >nul 2>&1
 mkdir "%_NEW%" >nul 2>&1
 
 del tmp1.txt >nul 2>&1
 del tmp2.txt >nul 2>&1
 del /f "%_NEW%\*.xml" >nul 2>&1
 
-for /r "%_INPUT%" %%f in (*.esp, *.esm) do ( echo %%f | findstr /v "%EXCLUDE%" ) >> tmp1.txt
+for /r "%_INPUT%" %%f in (*.esp, *.esm) do echo %%f >> tmp1.txt
 for /f "delims=" %%x in (tmp1.txt) do ( echo | set /p name=" "%%x" " ) >> tmp2.txt
 for /f "delims=" %%x in (tmp2.txt) do ( yampt.exe --make-not -a -f %%x -d "%DICT_N%" )
 for /f "delims=" %%x in (tmp2.txt) do ( yampt.exe --make-changed -a -f %%x -d "%DICT_F%" )
