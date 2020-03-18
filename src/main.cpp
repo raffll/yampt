0a8d5f071dbc05727614ec8af29fb2c420fc07e1
raffll <rafal.wierzchos@gmail.com>
2020-03-18 09:59:32 +0100

    add top level exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include "userinterface.hpp"
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    std::vector<std::string> arg;
-    for(int i = 0; i < argc; i++)
+    try
     {
-        arg.push_back(argv[i]);
+        std::vector<std::string> arg;
+        for (int i = 0; i < argc; i++)
+        {
+            arg.push_back(argv[i]);
+        }
+        UserInterface ui(arg);
     }
-    UserInterface ui(arg);
+    catch (...)
+    {
+        std::cout << "Unknown error!" << std::endl;
+    }
 }
