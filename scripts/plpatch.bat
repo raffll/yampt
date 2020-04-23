894a762ce107f72c4868d79ddbd6d342b0c561fa
raffll <rafal.wierzchos@gmail.com>
2020-04-23 23:00:09 +0200

    refactoring

--- scripts/plpatch.bat
+++ scripts/plpatch.bat
@@ -1,10 +1,10 @@
 @echo off
 
-SET DATA=..\Data Files
-SET BASE=dict_base
-SET USER=dict_user
-SET DICT_N=%BASE%\ENtoPL_plpatch.xml
-SET SUFFIX= (plpatch)
+set DATA=..\Data Files
+set BASE=dict_base
+set USER=dict_user
+set DICT_N=%BASE%\ENtoPL_plpatch.xml
+set SUFFIX= (plpatch)
 
 REM ############### DON'T EDIT ###############
 
@@ -13,16 +13,22 @@ mkdir "%BASE%" >nul 2>&1
 mkdir "%USER%" >nul 2>&1
 
 del /f "%DATA%\*%SUFFIX%*" >nul 2>&1
-del mods.txt >nul 2>&1
-del *.log >nul 2>&1
 
 ren "%DATA%\Morrowind.esm.bak" Morrowind.esm
 
-for /r "%DATA%" %%f in (*.esp, *.esm) do ( echo | set /p name=" "%%f" " ) >> mods.txt
-for /f "delims=" %%x in (mods.txt) do ( yampt.exe --convert --add-hyperlinks --windows-1250 -f %%x -d "%DICT_N%" %USER%\*.xml -s "%SUFFIX%" )
+setlocal enabledelayedexpansion enableextensions
+
+set DICT_U=
+for %%x in ("%USER%\*.xml") do set DICT_U=!DICT_U! "%%x"
+set DICT_U=%DICT_U:~1%
+
+set ESM=
+for %%f in ("%DATA%\*.esp", "%DATA%\*.esm") do set ESM=!ESM! "%%f"
+set ESM=%ESM:~1%
+
+yampt.exe --convert --add-hyperlinks --windows-1250 -f %ESM% -d "%DICT_N%" %DICT_U% -s "%SUFFIX%"
 
 ren "%DATA%\Morrowind.esm" Morrowind.esm.bak
-del mods.txt >nul 2>&1
 
 move /y "*.esm" "%DATA%" >nul 2>&1
 move /y "*.esp" "%DATA%" >nul 2>&1
